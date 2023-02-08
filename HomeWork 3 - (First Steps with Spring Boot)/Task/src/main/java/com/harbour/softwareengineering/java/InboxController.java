34a3df1e15efcd0cb4a07c0d41b94f430dee5f76
Yordán Luis Laborde Peña <yordanluislp@gmail.com>
2023-02-08 02:22:48 +0100

    solved task 3

--- HomeWork 3 - (First Steps with Spring Boot)/Task/src/main/java/com/harbour/softwareengineering/java/InboxController.java
+++ HomeWork 3 - (First Steps with Spring Boot)/Task/src/main/java/com/harbour/softwareengineering/java/InboxController.java
@@ -1,2 +1,25 @@
-package com.harbour.softwareengineering.java;public class InboxController {
+package com.harbour.softwareengineering.java;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class InboxController {
+    private final List<Item> Items = List.of(new Item(1, "Book", "Alice in Wonderland"), new Item(2, "Toothpaste", "Colgate"));
+    @GetMapping("/inbox/items")
+    public List<Item> items(){
+        return  Items;
+    }
+    @GetMapping("/inbox/items/{id}")
+    public Item item( @PathVariable("id")  int id ){
+        var items = Items.stream().filter(item -> item.getId() == id).collect(Collectors.toList());
+        if( items.size() > 0 )
+            return items.stream().findFirst().get();
+        return null;
+    }
 }
--- HomeWork 3 - (First Steps with Spring Boot)/Task/src/main/java/com/harbour/softwareengineering/java/Item.java
+++ HomeWork 3 - (First Steps with Spring Boot)/Task/src/main/java/com/harbour/softwareengineering/java/Item.java
@@ -1,2 +1,22 @@
-package com.harbour.softwareengineering.java;public class Item {
+package com.harbour.softwareengineering.java;
+
+public class Item {
+    private final int Id;
+    private final String Name;
+    private final String Description;
+
+    public int getId(){
+        return this.Id;
+    }
+    public String getName(){
+        return this.Name;
+    }
+    public String getDescription(){
+        return this.Description;
+    }
+    public Item( int id, String name, String description){
+        Id = id;
+        Name = name;
+        Description = description;
+    }
 }
